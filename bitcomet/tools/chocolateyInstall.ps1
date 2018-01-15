c7c88725d3ffed44060c506a49af41446ea66d58
Rustem Khayrullin <rhairullin@direct-credit.ru>
2018-01-15 17:35:35 +0300

    fix BitComet 1.47

--- bitcomet/tools/chocolateyInstall.ps1
+++ bitcomet/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿#try {
   $downUrl = 'https://download.bitcomet.com/achive/BitComet_1.47_setup.exe'
   # installer, will assert administrative rights
-  Install-ChocolateyPackage 'bzeditor' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
+  Install-ChocolateyPackage 'bitcomet' 'EXE' '/S' "$downUrl" -validExitCodes @(0)
   
   # the following is all part of error handling
-  #Write-ChocolateySuccess 'bzeditor'
+  #Write-ChocolateySuccess 'bitcomet'
 #} catch {
-  #Write-ChocolateyFailure 'bzeditor' "$($_.Exception.Message)"
+  #Write-ChocolateyFailure 'bitcomet' "$($_.Exception.Message)"
   #throw 
 #}
