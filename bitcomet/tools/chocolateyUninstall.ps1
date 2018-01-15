59485188d0ba88ce850ae2ca8e1fe2994890c12f
Rustem Khayrullin <rhairullin@direct-credit.ru>
2018-01-15 17:51:41 +0300

    fix BitComet 1.47

--- bitcomet/tools/chocolateyUninstall.ps1
+++ bitcomet/tools/chocolateyUninstall.ps1
@@ -1,11 +1,6 @@
 ﻿try {
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  if ($is64bit) {
-    $unpath = "${Env:ProgramFiles(x86)}\BitComet\uninst.exe"
-  } else {
-    $unpath = "${Env:ProgramFiles}\BitComet\uninst.exe"
-  }
+  $unpath = "${Env:ProgramFiles}\BitComet\uninst.exe"
+  
   Uninstall-ChocolateyPackage 'bitcomet' 'EXE' '/S' "$unpath" -validExitCodes @(0)
   
   # the following is all part of error handling
